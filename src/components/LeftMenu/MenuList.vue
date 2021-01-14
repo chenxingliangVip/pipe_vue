f3cc327652f06cd253e70653d37a4677a1eb6808
zhanglu97 <1067351553@qq.com>
2021-01-14 15:28:54 +0800

    左侧菜单问题

--- src/components/LeftMenu/MenuList.vue
+++ src/components/LeftMenu/MenuList.vue
@@ -10,55 +10,23 @@
                     <span slot="title">{{ item.title }}</span>
                 </template>
                 <el-menu-item-group>
-                    <Menu :menuData="item.children" :parentIndex=" parentIndex ? parentIndex + '-' + index : index "></Menu>
+                    <Menu :menuData="item.children" :parentIndex=" parentIndex ? parentIndex + '-' + index : index.toString() "></Menu>
                 </el-menu-item-group>
             </el-submenu>
-            <el-menu-item v-else-if="item.title == '物性计算'" :index=" parentIndex ? parentIndex + '-' + index : index.toString() " @click.native="computer()">
-                <i v-if="item.icon" :class="item.icon"></i>
-                <span slot="title">{{ item.title }}</span>
-            </el-menu-item>
             <el-menu-item v-else :index=" parentIndex ? parentIndex + '-' + index : index.toString() " @click.native="goPage(item.name)">
                 <i v-if="item.icon" :class="item.icon"></i>
                 <span slot="title">{{ item.title }}</span>
             </el-menu-item>
         </div>
-        
-        <!-- 物性速算弹框 -->
-        <div class="zll-dialog">
-            <popout title="物性速算" :visible.sync="computerDialog" v-if="computerDialog" class="computer_dialog">
-                <Computer ref="add" slot="content" :isEdit="isEdit" :editData="programInfo"></Computer>
-            </popout>
-        </div>
     </div>
 </template>
 
 <script>
-import Computer from "@/views/DesignPage/computer.vue";
 export default {
     props: {
         menuData: {},
         parentIndex: {
-            default: "",
-        },
-        computerDialog: false,
-        isEdit: false,
-        programInfo: {
-          id: "",
-          custom: '',
-          programName: '',
-          acuteValue: '',
-          rough: '',
-          designer: '',
-          checker: '',
-          examiner: '',
-          approver: '',
-          averDegree: '',
-          averWind: '',
-          airDegree: '',
-          groundDegree: '',
-          groundDeep: '',
-          groundHeat: '',
-          airHeat: '',
+            default: 0,
         },
     },
     name: "Menu",
@@ -66,41 +34,10 @@ export default {
         goPage(name) {
             //页面跳转
             if (this.$route.name != name) {
-                if (name == "Statistics") {
-                    const { href } = this.$router.resolve({
-                        path: "/Statistics",
-                    });
-                    window.open(href, "_blank");
-                    return false;
-                } else if (name == "ProductLine2") {
-                    const { href } = this.$router.resolve({
-                        path: "/ProductLine2",
-                    });
-                    window.open(href, "_blank");
-                    return false;
-                } else if (name == "ProductLine") {
-                    const { href } = this.$router.resolve({
-                        path: "/ProductLine",
-                    });
-                    window.open(href, "_blank");
-                    return false;
-                } else if (name == "QcScreen") {
-                    const { href } = this.$router.resolve({
-                        path: "/QcScreen",
-                    });
-                    window.open(href, "_blank");
-                    return false;
-                }
                 this.$router.push({ name });
             }
         },
-        computer() {
-            this.computerDialog = true
-        }
     },
-    components: {
-      Computer,
-    }
 };
 </script>
 
